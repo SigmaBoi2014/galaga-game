d62db35db656eb51742e000fde27492a676e265f
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-05-07 07:54:04 -0700

    Add files via upload

--- galaga.py
+++ galaga.py
@@ -2,13 +2,23 @@ import pgzrun
 WIDTH=470
 
 HEIGHT=470
+bull=[]
+bugs=[]
 ship=Actor("galaga")
 ship.pos=235,400
+
+for i in range(5):
+    bug=Actor("bug")
+    bug.x = 100+70*i
+    bug.y = 80
+    bugs.append(bug)
 def on_key_down(key):
     if key == keys.SPACE:
         bullet=Actor("bullet")
         bullet.x = ship.x
-        bullet.y = ship.y
+        bullet.y = ship.y-30
+        bull.append(bullet)
+
 
 def update():
     if(keyboard.left):
@@ -19,10 +29,17 @@ def update():
         ship.x+=10
         if(ship.x>470):
             ship.x=420
+
+    for b in bull:
+        b.y-=5
 def draw():
     screen.fill("blue")
     ship.draw()
-    bullet.draw()
+    for i in bull:
+        i.draw()
+    for b in bugs:
+        b.draw()
+    screen.draw.text("0",(0,0))
 
 
 pgzrun.go()
